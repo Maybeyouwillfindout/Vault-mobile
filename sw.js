87d8c5126f83617f9554eca28dd18114c119958d
Maybeyouwillfindout <timoruettener@hotmail.com>
2025-08-31 01:32:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE='vault-cache-v2-6-4';
-const ASSETS=['./','./index.html?v=2640','./styles.css?v=2640','./app.js?v=2640','./manifest.webmanifest?v=2640'];
+const CACHE='vault-cache-v2-6-5';
+const ASSETS=['./','./index.html?v=2650','./styles.css?v=2650','./app.js?v=2650','./manifest.webmanifest?v=2650'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
